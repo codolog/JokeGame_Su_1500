c37cacc4e649fa467a1d2462428cb568824bfbf6
codolog <77565375+codolog@users.noreply.github.com>
2021-01-31 15:57:51 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -12,27 +12,34 @@ clock = pg.time.Clock()
 
 class Button:
     # Конструктор
-    def __init__(self, color, x, y, width, height):
+    def __init__(self, color, x, y, width, height, sc):
         self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.screen = sc
         
     def draw(self):
-        print("Hello from draw()")
+        pg.draw.rect(self.screen, self.color, \
+                     (self.x, self.y, self.width, self.height), \
+                     0, border_radius=4)
 
-btn_yes = Button()
+WHITE = (255, 255, 255)
+RED = (0, 200, 200)
+btn_yes = Button(color=RED, x=100, y=100, width=100, height=30, sc=screen)
 
 running = True
 while running:
     clock.tick(FPS)
+    screen.fill(WHITE)
     
     list_events = pg.event.get()
     for event in list_events:
         if event.type == pg.QUIT:
             running = False
     
+    btn_yes.draw()
     pg.display.update()
 
 pg.quit()
